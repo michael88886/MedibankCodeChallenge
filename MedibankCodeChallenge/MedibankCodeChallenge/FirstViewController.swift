0711d6bebdf914f7b16426391b7ec5c024bc03a8
Michael Ho <50987398+michael88886@users.noreply.github.com>
2020-08-15 23:22:33 +1000

    project init

---
+++ MedibankCodeChallenge/MedibankCodeChallenge/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MedibankCodeChallenge
+//
+//  Created by mk mk on 15/8/20.
+//  Copyright © 2020 Michael.H. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MedibankCodeChallenge/MedibankCodeChallenge/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MedibankCodeChallenge
+//
+//  Created by mk mk on 15/8/20.
+//  Copyright © 2020 Michael.H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
