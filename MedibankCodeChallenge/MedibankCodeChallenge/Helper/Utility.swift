c957f4cb21df9d20fdff4f8dba94eededb7757b2
Michael Ho <50987398+michael88886@users.noreply.github.com>
2020-08-17 15:29:34 +1000

    Updated utility functions
    Up

--- MedibankCodeChallenge/MedibankCodeChallenge/Helper/Utility.swift
+++ MedibankCodeChallenge/MedibankCodeChallenge/Helper/Utility.swift
@@ -11,4 +11,23 @@ final class Utility {
         // Data context
         return appDelegate.persistentContainer.viewContext
     }
+    
+    /// Will convert two letter into enmoji icon
+    /// Credit:  https://stackoverflow.com/a/60413173
+    static func toFlag(_ countryCode: String) -> String {
+        let base: UInt32 = 127397
+        return countryCode.uppercased()
+            .unicodeScalars
+            .map({ base + $0.value })
+            .compactMap(UnicodeScalar.init)
+            .map(String.init)
+            .joined()
+    }
+    
+    /// Generic error alert
+    static func errorAlert(with title: String, message: String) -> UIAlertController {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        return alertVC
+    }
 }
